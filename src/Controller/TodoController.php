36068d7171f2a5e82806a0dfe984b8525e362e75
Areebse <areeb.shahzad@devsinc.com>
2023-08-01 20:40:41 +0500

    added create todo functionality

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -1,32 +1,63 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Todo;
+use App\Form\NewTodoType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
 //    #[Route('/todo', name: 'app_todo')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        $user = $this->getUser()->getEmail();
+        $todoList = $entityManager->getRepository(Todo::class)->findAll();
+/////////
+//        $user = $this->getUser()->getEmail();
+//        We can get user details using this
+/////////
         return $this->render('todo/index.html.twig', [
-            'controller_name' => $user,
+            'todo_list' => $todoList,
         ]);
     }
 
-    public function show(){
+    #[Route('/createTodoView', name: 'create_todo_view')]
+    public function viewCreateTodo(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $todo = new Todo();
+        $form = $this->createForm(NewTodoType::class,$todo);
+        $form->handleRequest($request);
+//        dump($form);
+//        exit();
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $userId = $this->getUser();
+            $todo->setName(
+                $form->get('name')->getData()
+            );
+            $todo->setUserId($userId);
+
+            $entityManager->persist($todo);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+//        dump($user);
+            $url = $this->generateUrl('app_todo');
+            return $this->redirect($url);
+        }
+       return $this->render('/todo/createTodo.html.twig',['todoForm'=>$form]);
 
     }
 
     public function add(){
 
     }
-
-    public function delete(){
-
+    #[Route('/deleteTodo', name: 'delete_todo')]
+    public function delete(Request $request, EntityManagerInterface $entityManager){
+      dump($request);
+      exit();
     }
 
     public function update(){
